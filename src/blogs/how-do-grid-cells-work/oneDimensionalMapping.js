90057eb0aeb09109a5ae208a622f3bf070e47c6f
rhyolight <rhyolight@gmail.com>
2018-05-04 12:36:47 -0700

    Tweaks

--- src/blogs/how-do-grid-cells-work/oneDimensionalMapping.js
+++ src/blogs/how-do-grid-cells-work/oneDimensionalMapping.js
@@ -1,73 +1,30 @@
 let utils = require('../../widgets/utils')
 let html = require('./oneDimensionalMapping.tmpl.html')
 
-// from http://bl.ocks.org/mbostock/4349187
-// Sample from a normal distribution with mean 0, stddev 1.
-function normal() {
-    let x = 0,
-        y = 0,
-        rds, c
-    do {
-        x = Math.random() * 2 - 1
-        y = Math.random() * 2 - 1
-        rds = x * x + y * y
-    } while (rds == 0 || rds > 1)
-    c = Math.sqrt(-2 * Math.log(rds) / rds) // Box-Muller transform
-    return x * c // throw away extra sample y * c
+let clrs = {
+    barFill: 'lightgrey',
+    barStroke: 'grey',
+    curveFill: 'salmon',
+    curveStroke: 'red',
+    anchorStroke: 'black',
+    anchorFill: 'none',
+    rangeStroke: 'black'
 }
-
-//taken from Jason Davies science library
-// https://github.com/jasondavies/science.js/
-function gaussian(x) {
-    let gaussianConstant = 1 / Math.sqrt(2 * Math.PI),
-        mean = 0,
-        sigma = 1
-
-    x = (x - mean) / sigma
-    return gaussianConstant * Math.exp(-.5 * x * x) / sigma
-}
-
-function getData() {
-    let data = []
-    // loop to populate data array with
-    // probabily - quantile pairs
-    for (var i = 0; i < 10000; i++) {
-        q = normal() // calc random draw from normal dist
-        p = gaussian(q) // calc prob of rand draw
-        el = {
-            "q": q,
-            "p": p
-        }
-        data.push(el)
-    }
-
-    // need to sort for plotting
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-    data.sort(function(x, y) {
-        return x.q - y.q
-    })
-    return data
-}
-
-let data = getData()
-let dataMax = 0
-data.forEach(d => {
-    if (d.p > dataMax) dataMax = d.p
-})
 
 module.exports = (elId) => {
     utils.loadHtml(html.default, elId, () => {
 
-        let scale = 1.0
-        let anchor = 0.0
-        let sensitivity = 1.0
-        let range = 0.1
+        let scale = .2
+        let anchor = .5
+        let range = 0.5
+        let independentVariables = 6
 
-        let width = 560
+        let width = 450
         let height = 100
 
-        let upperHeight = 60
+        let upperHeight = 40
         let barHeight = 30
+        let histTicks = 50
 
         let $anchorSlider = $('#anchorPerc')
         $anchorSlider.val(parseInt(anchor * 100))
@@ -75,6 +32,8 @@ module.exports = (elId) => {
         $rangeSlider.val(parseInt(range * 100))
         let $scaleSlider = $('#scalePerc')
         $scaleSlider.val(parseInt(scale * 100))
+        let $independentVarsSlider = $('#independentVars')
+        $independentVarsSlider.val(parseInt(independentVariables))
 
         let $svg = d3.select('#' + elId + ' svg')
                     .attr('width', width)
@@ -86,21 +45,36 @@ module.exports = (elId) => {
             .attr('width', width)
             .attr('height', barHeight)
             .attr('y', upperHeight)
-            .attr('stroke', 'black')
-            .attr('stroke-width', '2px')
-            .attr('fill', 'none')
+            .attr('stroke', clrs.barStroke)
+            .attr('fill', clrs.barFill)
 
         // Wireframe group
         let $wireGroup = $svg.append('g')
             .attr('class', 'wires')
 
-        let $sensitivityCurve = $wireGroup.append('path')
-            .attr('class', 'sensitivity')
-            .attr('stroke', 'black')
-            .attr('fill', 'none')
+        // let $marker = $svg.append('rect').attr('class', 'marker')
 
-        let $marker = $svg.append('rect').attr('class', 'marker')
+        // Playing with data
+        let data = createDistribution()
 
+        function createDistribution() {
+            let out = d3.range(50000)
+                .map(d3.randomBates(independentVariables))
+                .map((val, i) => {
+                    return val - 0.5
+                })
+            let equalizer = d3.scaleLinear()
+                .domain([Math.min(...out), Math.max(...out)])
+                .range([0, 1])
+            out = out.map(d => equalizer(d))
+            return out
+        }
+
+        //
+        // Treatments and updates
+        //
+
+        // Ranges
         function treatRanges($ranges, rangeCaps, x, y, radius) {
             let x1 = x - radius
             let x2 = x + radius
@@ -108,7 +82,7 @@ module.exports = (elId) => {
 
             $ranges
                 .attr('class', 'range')
-                .attr('stroke', 'black')
+                .attr('stroke', clrs.rangeStroke)
                 .attr('stroke-width', '1px')
                 .attr('stroke-dasharray', [3, 1])
                 .attr('x1', x1)
@@ -117,14 +91,14 @@ module.exports = (elId) => {
                 .attr('y2', y)
             rangeCaps.left
                 .attr('class', 'left')
-                .attr('stroke', 'black')
+                .attr('stroke', clrs.rangeStroke)
                 .attr('x1', x1)
                 .attr('y1', y - barHeight / rangeCapRatio)
                 .attr('x2', x1)
                 .attr('y2', y + barHeight / rangeCapRatio)
             rangeCaps.right
                 .attr('class', 'right')
-                .attr('stroke', 'black')
+                .attr('stroke', clrs.rangeStroke)
                 .attr('x1', x2)
                 .attr('y1', y - barHeight / rangeCapRatio)
                 .attr('x2', x2)
@@ -157,37 +131,67 @@ module.exports = (elId) => {
             rangeCaps.right.exit().remove()
         }
 
-        function updateSensitivityCurve(x, y, $sensitivityCurve) {
+        // Curves
+        function treatSensitivityCurves(curves, lineData) {
+            curves
+                .attr('class', 'sensitivity')
+                .attr('stroke', clrs.curveStroke)
+                .attr('fill', clrs.curveFill)
+                .attr('d', lineData)
+        }
+
+        function updateSensitivityCurve(wires, x, y) {
+
             let centerX = (x + (anchor * width)) * scale
             let rangePix = width * range * scale
             let radius = rangePix / 2
             let xLeft = centerX - radius
             let xRight = (centerX + radius)
 
-            let first = data[0]
-            let last = data[data.length - 1]
+            let binScale = d3.scaleLinear()
+                .rangeRound([xLeft, xRight]);
+
+            let bins = d3.histogram()
+                .domain(binScale.domain())
+                .thresholds(binScale.ticks(histTicks))
+                (data);
+
+            let binMax = Math.max(...bins.map(b => b.length))
+
             let xScale = d3.scaleLinear()
-                .domain([first.q, last.q])
+                .domain([bins[0].x0, bins[bins.length-1].x0])
                 .range([xLeft, xRight])
+            // Using the data max to fill height of frame
             let yScale = d3.scaleLinear()
-                .domain([0, dataMax])
+                .domain([0, binMax])
                 .range([upperHeight, 0])
 
             let lineFunction = d3.line()
-                .x(function(d) { return xScale(d.q) })
-                .y(function(d) { return yScale(d.p) })
-                .curve(d3.curveCatmullRom.alpha(0.5))
+                .x(d => xScale(d.x0))
+                .y(d => yScale(d.length))
+            let lineData = lineFunction(bins)
 
-            $sensitivityCurve.attr('d', lineFunction(data))
+            // Update
+            let $sensitivityCurves = wires.selectAll('path.sensitivity')
+            treatSensitivityCurves($sensitivityCurves, lineData)
+
+            // Enter
+            let $newCurves = $sensitivityCurves.data([null]).enter().append('path')
+            treatSensitivityCurves($newCurves, lineData)
+
+            // Exit
+            $sensitivityCurves.exit().remove()
         }
 
+        // Anchors
         function treatAnchors(anchors) {
             anchors
                 .attr('class', 'anchor')
                 .attr('r', 3)
                 .attr('cx', d => d[0])
                 .attr('cy', d => d[1])
-                .attr('fill', 'black')
+                .attr('fill', clrs.anchorFill)
+                .attr('stroke', clrs.anchorStroke)
         }
 
         function updateAnchors(wires, x, y) {
@@ -204,6 +208,7 @@ module.exports = (elId) => {
             $anchors.exit().remove()
         }
 
+        // Wires, which are groups of all the treatments above
         function treatWires(wires, x, y) {
             wires.attr('class', 'wire')
                 .attr('id', d => 'wire-' + d)
@@ -213,10 +218,13 @@ module.exports = (elId) => {
                 })
             updateAnchors(wires, x, y)
             updateRange(wires, x, y)
+            updateSensitivityCurve(wires, x, y)
         }
 
+        // Update the entire display, should be called on UI value change or on
+        // animation frame update
         function updateDisplay() {
-            let repetitions = d3.range(0, Math.ceil(1 / scale))
+            let repetitions = d3.range(-1, Math.ceil(1 / scale) + 1)
             let x = 0
             let y = upperHeight
 
@@ -231,12 +239,21 @@ module.exports = (elId) => {
 
             // Exit
             $wires.exit().remove()
+
+            // Update sliders
+            $anchorSlider.val(anchor * 100)
+            $rangeSlider.val(range * 100)
+            $scaleSlider.val(scale * 100)
+            $independentVarsSlider.val(independentVariables)
         }
 
+        // This is the input from the user. Values change and the display updates.
         $('#' + elId + ' input').on('input', () => {
             anchor = parseInt($anchorSlider.val()) / 100
             range = parseInt($rangeSlider.val()) / 100
             scale = parseInt($scaleSlider.val()) / 100
+            independentVariables = parseInt($independentVarsSlider.val())
+            data = createDistribution()
             updateDisplay()
         })
 
