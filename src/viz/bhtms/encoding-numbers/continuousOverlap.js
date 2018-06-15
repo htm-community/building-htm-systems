48f3a78e2a28ff1c0db72f70d974d0c14081b2ae
rhyolight <rhyolight@gmail.com>
2018-06-15 09:29:13 -0700

    Continuous overlap first pass complete

--- src/viz/bhtms/encoding-numbers/continuousOverlap.js
+++ src/viz/bhtms/encoding-numbers/continuousOverlap.js
@@ -9,7 +9,7 @@ module.exports = (elementId, val1, val2, encoderCfg) => {
         let encoder = new BoundedScalarEncoder(encoderCfg)
 
         let width = 560,
-            height = 100,
+            height = 140,
             gutter = 20,
             uiRange = [gutter, width - gutter],
             rectWidth = (uiRange[1] - uiRange[0]) / encoder.n,
@@ -30,8 +30,83 @@ module.exports = (elementId, val1, val2, encoderCfg) => {
             return [e, encoding2[i]]
         })
 
-        function drawBrackets(barPoint1, barPoint2, labelCenter) {
-            
+        let lineFunction = d3.line()
+            .x(function(d) { return d.x; })
+            .y(function(d) { return d.y; })
+            .curve(d3.curveCatmullRom.alpha(0.5));
+
+        function drawBracket(value, i) {
+            let valueScaleTopMargin = 30
+
+            let $bracketGroup = $svg.select('.val' + i),
+                $leftPath = $bracketGroup.select('path.left'),
+                $rightPath = $bracketGroup.select('path.right'),
+                $label = $bracketGroup.select('text')
+
+
+
+            let leftLineData = []
+            let rightLineData = []
+            let index = Math.floor(encoder.scale(value))
+            let w = encoder.w
+            let leftIndex = encoder.reverseScale(index - (w/2)),
+                leftValue = encoder.scale(leftIndex),
+                rightIndex = encoder.reverseScale(index + (w/2)),
+                rightValue = encoder.scale(rightIndex)
+
+            let cx = gutter + index * rectWidth // center x
+            let cy = rectHeight * 2.2 // center y
+
+            $label.attr('x', cx)
+                .attr('y', cy + 15)
+                .html(value)
+
+            leftLineData.push({x: cx, y: cy})
+            rightLineData.push({x: cx, y: cy})
+            let nearX = xScale(leftValue)
+            let farX = xScale(rightValue)
+            // Intermediary points for curving
+            leftLineData.push({
+                x: cx - 10,
+                y: cy - 10,
+            })
+            leftLineData.push({
+                x: nearX,
+                y: valueScaleTopMargin + 10
+            })
+            rightLineData.push({
+                x: cx + 10,
+                y: cy - 10,
+            })
+            rightLineData.push({
+                x: farX,
+                y: valueScaleTopMargin + 10
+            })
+
+            // Point on value line
+            leftLineData.push({
+                x: nearX,
+                y: valueScaleTopMargin
+            })
+            rightLineData.push({
+                x: farX,
+                y: valueScaleTopMargin
+            })
+            $leftPath
+                .attr('d', lineFunction(leftLineData))
+                .attr('stroke', 'black')
+                .attr('fill', 'none')
+            $rightPath
+                .attr('d', lineFunction(rightLineData))
+                .attr('stroke', 'black')
+                .attr('fill', 'none')
+            $bracketGroup.attr('visibility', 'visible')
+
+        }
+
+        function drawBrackets() {
+            drawBracket(val1, 1)
+            drawBracket(val2, 2)
         }
 
         function treatRects(rects) {
@@ -64,6 +139,8 @@ module.exports = (elementId, val1, val2, encoderCfg) => {
         treatRects(newRects)
 
         rects.exit().remove()
+
+        drawBrackets()
 
     })
 
