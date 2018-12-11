6daf7de7643cabc841da93b1a19bd7478c04a9e2
rhyolight <matt@numenta.org>
2018-12-11 14:03:40 -0800

    Added a research map

---
+++ src/widgets/blogs/research-map/index.js
@@ -0,0 +1,108 @@
+let utils = require('../../../lib/utils')
+let html = require('./researchMap.tmpl.html')
+let rawMap = require('./researchMap.json')['Research Map']
+let selected = '1000 Brains'
+let $el, $ul, $content
+
+function extractNode(name, map) {
+    // console.log('extracting %s', name)
+    let out
+    if (map[name]) {
+        out = map[name]
+        out.name = name
+    } else {
+        let topics = Object.keys(map)
+        topics.forEach(topicName => {
+            let children = map[topicName].children
+            if (children) {
+                out = extractNode(name, children)
+                if (out) {
+                    out.parent = map[topicName]
+                    out.parent.name = topicName
+                }
+            }
+        })
+    }
+    return out
+}
+
+function renderContent(node) {
+    if (node.desc) {
+        $content.html(node.desc)
+    }
+    if (node.resources) {
+        $content.append('<h3>Resources:')
+        let $res = $('<ul class="resources">')
+        Object.keys(node.resources).forEach(name => {
+            let url = node.resources[name]
+            $res.append(
+                '<li><a href="' + url + '">' + name + '</a></li>'
+            )
+        })
+        $content.append($res)
+    }
+    if (node.children) {
+        $content.append('<h3>Explore:')
+        let $res = $('<ul class="children topics">')
+        Object.keys(node.children).forEach(name => {
+            $res.append('<li class="topic">' + name)
+        })
+        $content.append($res)
+    }
+}
+
+function renderResearchMap(toSelect) {
+    console.log('selecting %s', toSelect)
+    let node = extractNode(toSelect, rawMap)
+    // Clear list
+    $ul.html('')
+    // Display up node
+    let parentName = node.name
+    if (node.parent) {
+        parentName = node.parent.name
+        console.log('parent: %s', parentName)
+    }
+    let clazz = 'up '
+    if (parentName === toSelect) {
+        clazz += 'selected'
+    }
+    $ul.append('<li class="' + clazz + '">' + parentName)
+    // Display sibling nodes
+    let siblings = node.parent ? node.parent.children : node.children
+    Object.keys(siblings).forEach(key => {
+        let clazz = 'topic '
+        if (key === toSelect) {
+            clazz += 'selected'
+        }
+        $ul.append('<li class="' + clazz + '">' + key)
+    })
+    renderContent(node)
+    console.log(toSelect)
+    console.log(node)
+}
+
+function setupListeners() {
+    $el.on('mousemove', (evt => {
+        // console.log(evt.target)
+        if ($(evt.target).hasClass('topic')) {
+            if (evt.target.innerHTML) {
+                renderResearchMap(evt.target.innerHTML)
+            }
+        }
+    }))
+}
+
+function researchMap(elId) {
+    utils.loadHtml(html.default, 'research-map', () => {
+        $el = $('#' + elId)
+        $ul = $el.find('ul.topics')
+        $content = $el.find('.topic-content')
+        renderResearchMap(selected)
+        setupListeners()
+    })
+}
+
+window.BHTMS = {
+    researchMap: researchMap,
+    JSDS: require('JSDS'),
+}
--- webpack.config.js
+++ webpack.config.js
@@ -10,213 +10,213 @@ let mode = 'development'
 
 let modules = []
 
-/*******************************
- * ENCODERS
- *******************************/
-
-// encoding-numbers/
-modules.push({
-    mode: mode,
-    entry: [
-        './src/widgets/bhtms/encoding-numbers/index.js',
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /tmpl\.html$/,
-                loader: 'posthtml-loader'
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            'dat.gui': path.join(__dirname, 'node_modules/dat.gui/build/dat.gui.min'),
-            JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
-            SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
-            SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
-            ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
-            BoundedScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/boundedScalar'),
-            CategoryEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/category'),
-            CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
-            FancyCyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/fancyCyclicEncoderDisplay'),
-        }
-    },
-    output: {
-        path: __dirname + '/wp-content/uploads/encoding-numbers',
-        filename: `bhtms-encoding-numbers-${version}.js`
-    }
-})
-
-// encoding-categories
-modules.push({
-    mode: mode,
-    entry: [
-        './src/widgets/bhtms/encoding-categories/index.js',
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /tmpl\.html$/,
-                loader: 'posthtml-loader'
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            moment: path.join(__dirname, 'node_modules/moment/min/moment.min'),
-            JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
-            SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
-            SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
-            // ScalarEncoder: path.join(__dirname, '../simplehtm/src/encoders/scalar'),
-            // CyclicEncoder: path.join(__dirname, '../simplehtm/src/encoders/cyclicScalar'),
-            ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
-            CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
-            CategoryEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/category'),
-            WeekendEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/weekend'),
-            CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/cyclicEncoderDisplay'),
-            CategoryEncoderDisplay: path.join(__dirname, 'src/lib/displays/categoryEncoderDisplay'),
-        }
-    },
-    output: {
-        path: __dirname + '/wp-content/uploads/encoding-categories',
-        filename: `bhtms-encoding-categories-${version}.js`
-    }
-})
-
-// encoding-time/
-// This is bundled to be hosted with images
-modules.push({
-    mode: mode,
-    entry: [
-        './src/widgets/bhtms/encoding-time/index.js',
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /tmpl\.html$/,
-                loader: 'posthtml-loader'
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'images',
-                    publicPath: '../../../../wp-content/uploads/encoding-time/images/',
-                }
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            moment: path.join(__dirname, 'node_modules/moment/min/moment.min'),
-            JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
-            SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
-            CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
-            CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/CyclicEncoderDisplay'),
-        }
-    },
-    output: {
-        path: __dirname + '/wp-content/uploads/encoding-time',
-        filename: `bhtms-encoding-time-${version}.js`
-    }
-})
-
-// combined-encoding/
-modules.push({
-    mode: mode,
-    entry: [
-        './src/widgets/bhtms/combined-encoding/index.js',
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /tmpl\.html$/,
-                loader: 'posthtml-loader'
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            'dat.gui': path.join(__dirname, 'node_modules/dat.gui/build/dat.gui.min'),
-            JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
-            SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
-            SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
-            ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
-            BoundedScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/boundedScalar'),
-            CategoryEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/category'),
-            CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
-            CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/cyclicEncoderDisplay'),
-        }
-    },
-    output: {
-        path: __dirname + '/wp-content/uploads/combined-encoding',
-        filename: `bhtms-combined-encoding-${version}.js`,
-    }
-})
-
-/*******************************
- * SPATIAL POOLING
- *******************************/
-
-// /input-space/
-modules.push({
-    mode: mode,
-    entry: [
-        './src/widgets/bhtms/input-space/index.js',
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /tmpl\.html$/,
-                loader: 'posthtml-loader'
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            moment: path.join(__dirname, 'node_modules/moment/moment'),
-            JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
-            SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
-            SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
-            ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
-            BoundedScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/boundedScalar'),
-            CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
-            CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/cyclicEncoderDisplay'),
-        }
-    },
-    output: {
-        path: __dirname + '/wp-content/uploads/input-space',
-        filename: `bhtms-input-space-${version}.js`
-    }
-})
-
-/*******************************
- * TEMPORAL MEMORY
- *******************************/
-
-// TODO
-
+// /*******************************
+//  * ENCODERS
+//  *******************************/
+//
+// // encoding-numbers/
+// modules.push({
+//     mode: mode,
+//     entry: [
+//         './src/widgets/bhtms/encoding-numbers/index.js',
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader'
+//             },
+//             {
+//                 test: /tmpl\.html$/,
+//                 loader: 'posthtml-loader'
+//             }
+//         ]
+//     },
+//     resolve: {
+//         alias: {
+//             'dat.gui': path.join(__dirname, 'node_modules/dat.gui/build/dat.gui.min'),
+//             JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
+//             SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
+//             SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
+//             ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
+//             BoundedScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/boundedScalar'),
+//             CategoryEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/category'),
+//             CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
+//             FancyCyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/fancyCyclicEncoderDisplay'),
+//         }
+//     },
+//     output: {
+//         path: __dirname + '/wp-content/uploads/encoding-numbers',
+//         filename: `bhtms-encoding-numbers-${version}.js`
+//     }
+// })
+//
+// // encoding-categories
+// modules.push({
+//     mode: mode,
+//     entry: [
+//         './src/widgets/bhtms/encoding-categories/index.js',
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader'
+//             },
+//             {
+//                 test: /tmpl\.html$/,
+//                 loader: 'posthtml-loader'
+//             }
+//         ]
+//     },
+//     resolve: {
+//         alias: {
+//             moment: path.join(__dirname, 'node_modules/moment/min/moment.min'),
+//             JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
+//             SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
+//             SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
+//             // ScalarEncoder: path.join(__dirname, '../simplehtm/src/encoders/scalar'),
+//             // CyclicEncoder: path.join(__dirname, '../simplehtm/src/encoders/cyclicScalar'),
+//             ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
+//             CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
+//             CategoryEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/category'),
+//             WeekendEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/weekend'),
+//             CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/cyclicEncoderDisplay'),
+//             CategoryEncoderDisplay: path.join(__dirname, 'src/lib/displays/categoryEncoderDisplay'),
+//         }
+//     },
+//     output: {
+//         path: __dirname + '/wp-content/uploads/encoding-categories',
+//         filename: `bhtms-encoding-categories-${version}.js`
+//     }
+// })
+//
+// // encoding-time/
+// // This is bundled to be hosted with images
+// modules.push({
+//     mode: mode,
+//     entry: [
+//         './src/widgets/bhtms/encoding-time/index.js',
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader'
+//             },
+//             {
+//                 test: /tmpl\.html$/,
+//                 loader: 'posthtml-loader'
+//             },
+//             {
+//                 test: /\.(png|svg|jpg|gif)$/,
+//                 loader: 'file-loader',
+//                 options: {
+//                     name: '[name].[ext]',
+//                     outputPath: 'images',
+//                     publicPath: '../../../../wp-content/uploads/encoding-time/images/',
+//                 }
+//             }
+//         ]
+//     },
+//     resolve: {
+//         alias: {
+//             moment: path.join(__dirname, 'node_modules/moment/min/moment.min'),
+//             JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
+//             SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
+//             CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
+//             CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/CyclicEncoderDisplay'),
+//         }
+//     },
+//     output: {
+//         path: __dirname + '/wp-content/uploads/encoding-time',
+//         filename: `bhtms-encoding-time-${version}.js`
+//     }
+// })
+//
+// // combined-encoding/
+// modules.push({
+//     mode: mode,
+//     entry: [
+//         './src/widgets/bhtms/combined-encoding/index.js',
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader'
+//             },
+//             {
+//                 test: /tmpl\.html$/,
+//                 loader: 'posthtml-loader'
+//             }
+//         ]
+//     },
+//     resolve: {
+//         alias: {
+//             'dat.gui': path.join(__dirname, 'node_modules/dat.gui/build/dat.gui.min'),
+//             JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
+//             SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
+//             SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
+//             ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
+//             BoundedScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/boundedScalar'),
+//             CategoryEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/category'),
+//             CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
+//             CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/cyclicEncoderDisplay'),
+//         }
+//     },
+//     output: {
+//         path: __dirname + '/wp-content/uploads/combined-encoding',
+//         filename: `bhtms-combined-encoding-${version}.js`,
+//     }
+// })
+//
+// /*******************************
+//  * SPATIAL POOLING
+//  *******************************/
+//
+// // /input-space/
+// modules.push({
+//     mode: mode,
+//     entry: [
+//         './src/widgets/bhtms/input-space/index.js',
+//     ],
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader'
+//             },
+//             {
+//                 test: /tmpl\.html$/,
+//                 loader: 'posthtml-loader'
+//             }
+//         ]
+//     },
+//     resolve: {
+//         alias: {
+//             moment: path.join(__dirname, 'node_modules/moment/moment'),
+//             JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
+//             SdrUtils: path.join(__dirname, 'node_modules/cell-viz/src/SdrUtils'),
+//             SdrDrawing: path.join(__dirname, 'node_modules/cell-viz/src/SdrDrawing'),
+//             ScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/scalar'),
+//             BoundedScalarEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/boundedScalar'),
+//             CyclicEncoder: path.join(__dirname, 'node_modules/simplehtm/src/encoders/cyclicScalar'),
+//             CyclicEncoderDisplay: path.join(__dirname, 'src/lib/displays/cyclicEncoderDisplay'),
+//         }
+//     },
+//     output: {
+//         path: __dirname + '/wp-content/uploads/input-space',
+//         filename: `bhtms-input-space-${version}.js`
+//     }
+// })
+//
+// /*******************************
+//  * TEMPORAL MEMORY
+//  *******************************/
+//
+// // TODO
+//
 /*******************************
  * GRID CELL BLOG
  *******************************/
@@ -247,9 +247,39 @@ modules.push({
         }
     },
     output: {
-        path: __dirname + '/docs',
+        path: __dirname + '/blogs',
         filename: `bhtms-how-grid-cells-map-space-${version}.js`
     }
 })
+
+// Research Map
+modules.push({
+    mode: mode,
+    entry: [
+        './src/widgets/blogs/research-map/index.js',
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /tmpl\.html$/,
+                loader: 'posthtml-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            JSDS: path.join(__dirname, 'node_modules/javascript-data-store/src/jsds'),
+        }
+    },
+    output: {
+        path: __dirname + '/blogs',
+        filename: `research-map-${version}.js`
+    }
+})
+
 
 module.exports = modules
