f814fd83e7653121faccac8c7d55d9f62033602c
rhyolight <matt@numenta.org>
2018-12-17 14:10:37 -0800

    Got jqueryui nested accordion building from yaml and interacting

--- src/widgets/blogs/research-map/prototype_2.js
+++ src/widgets/blogs/research-map/prototype_2.js
@@ -4,10 +4,6 @@ let YAML = require('yamljs')
 let rawMap = YAML.load('./research-map.yaml')
 
 
-function isLeaf(node) {
-    return node.children === undefined
-}
-
 function htmlNodeLoader(node, $el, _name) {
     // Read through hierarchy and create the HTML we need
     // to support the nested accordions
@@ -15,37 +11,30 @@ function htmlNodeLoader(node, $el, _name) {
     let $header = $('<h3>')
     let $content = $('<div>')
 
-    if (node.children) {
-        debugger;
-    } else {
-        // It's a map of children
-        $header.html('children:')
+    let nodeName = (_name || node.name)
+
+    if (! nodeName) {
         let childNames = Object.keys(node)
         let $ul = $('<ul class="accordion">')
         childNames.forEach((name, i) => {
-            console.log(name)
-            let $li = htmlNodeLoader(node[name], $('<li>'), name)
+            if (name === "desc") {
+
+            } else if (name === "resources") {
+            } else if (name === "requires") {
+
+            } else {
+                let $li = htmlNodeLoader(node[name], $('<li>'), name)
+                $ul.append($li)
+            }
         })
+        $content.html($ul)
+    } else if (node.children) {
+        $content = htmlNodeLoader(node.children, $content)
     }
 
-    // let name = node.name
-    // let $h3 = $('<h3>' + name)
-    // let $content = $('<div class="content">')
-    //
-    // if (node.summary) {
-    //     $out.append('<p>' + node.summary)
-    // }
-    //
-    // if (isLeaf(node)) {
-    //
-    // } else {
-    //     let $new = $('<ul class="accordion">')
-    //     node.children.forEach((child, i) => {
-    //         $new.append(htmlNodeLoader(node.children[i], $('<li>')))
-    //     })
-    //     $out.append($new)
-    // }
-    // $el.append($out)
+    $header.html(nodeName)
+
+    $el.append([$header, $content])
     return $el
 }
 
@@ -58,11 +47,11 @@ function render(elId) {
 
     // $el.accordion()
 
-    // $el.find("ul.accordion").accordion({
-    //     collapsible: true,
-    //     active: false,
-    //     heightStyle: "content"
-    // });
+    $el.find("ul.accordion").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content"
+    });
 }
 
 function researchMap(elId) {
