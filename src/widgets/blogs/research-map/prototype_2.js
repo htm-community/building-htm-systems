6157b436588fa2cd8ae3377f73173c5b33cf2769
Matthew Taylor <matt@numenta.org>
2018-12-17 16:22:21 -0800

    Nice hierarchy and interaction

--- src/widgets/blogs/research-map/prototype_2.js
+++ src/widgets/blogs/research-map/prototype_2.js
@@ -2,7 +2,7 @@ let utils = require('../../../lib/utils')
 let html = require('./prototype_2.tmpl.html')
 let YAML = require('yamljs')
 let rawMap = YAML.load('./research-map.yaml')
-
+let firstRendered = true
 
 function htmlNodeLoader(node, $el, _name) {
     // Read through hierarchy and create the HTML we need
@@ -13,23 +13,40 @@ function htmlNodeLoader(node, $el, _name) {
 
     let nodeName = (_name || node.name)
 
+    if (node.desc) {
+        $content.append('<p class="desc">' + node.desc)
+    }
+
     if (! nodeName) {
         let childNames = Object.keys(node)
-        let $ul = $('<ul class="accordion">')
+        let clazz = "accordion "
+        if (firstRendered) {
+            clazz += "top"
+            firstRendered = false
+        } else {
+            clazz += "below"
+        }
+        let $ul = $('<ul class="' + clazz + '">')
         childNames.forEach((name, i) => {
-            if (name === "desc") {
-
-            } else if (name === "resources") {
-            } else if (name === "requires") {
-
-            } else {
-                let $li = htmlNodeLoader(node[name], $('<li>'), name)
-                $ul.append($li)
-            }
+            $ul.append(htmlNodeLoader(node[name], $('<li>'), name))
         })
-        $content.html($ul)
+        $content.append($ul)
     } else if (node.children) {
-        $content = htmlNodeLoader(node.children, $content)
+        $content.append(htmlNodeLoader(node.children, $content))
+    }
+
+    if (node.resources) {
+        let $res = $('<ul>')
+        Object.keys(node.resources).forEach(resource => {
+            let url = node.resources[resource]
+            let $link = $('<a href="' + url + '" target="_blank">')
+            $link.html(resource)
+            let $li = $('<li>')
+            $li.append($link)
+            $res.append($li)
+        })
+        $content.append('<h4>Other Resources')
+        $content.append($res)
     }
 
     $header.html(nodeName)
@@ -44,14 +61,17 @@ function loadAccordionHtml(elId) {
 
 function render(elId) {
     let $el = loadAccordionHtml(elId)
-
-    // $el.accordion()
-
-    $el.find("ul.accordion").accordion({
+    $el.find("ul.accordion.below").accordion({
         collapsible: true,
         active: false,
         heightStyle: "content"
     });
+
+    $el.find("ul.accordion.first").accordion({
+        collapsible: false,
+        active: true,
+        heightStyle: "content"
+    })
 }
 
 function researchMap(elId) {
