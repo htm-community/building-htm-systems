af7cb0a28f595d36dbff69a4b15efa5d0f9e4cd3
Vratislav Kalenda <v.kalenda@gmail.com>
2019-04-15 19:37:59 +0200

    feat: add Code component

---
+++ pages/components-showcase.js
@@ -0,0 +1,43 @@
+import Layout from '../components/Layout'
+import Code from '../components/Code'
+
+
+const codeBlockContents =
+`const CategoryEncoder = require('./category')
+
+
+class WeekendEncoder extends CategoryEncoder {
+
+    constructor(opts) {
+        super({
+            w: opts.w,
+            categories: ['weekday', 'weekend'],
+        })
+    }
+
+    encode(date) {
+        let dayOfWeek = date.getDay()
+        let value = 'weekday'
+        if (dayOfWeek === 0 || dayOfWeek === 6)
+            value ='weekend'
+        return super.encode(value)
+    }
+
+}
+
+module.exports = WeekendEncoder
+`
+const highlightedLines = [5,6,8,10]
+
+export default function EncodingCategories() {
+  return (
+    <div>
+      <Layout>
+        <h2>Components Showcase</h2>
+        <i>This page demoes the available components for creating tutorials</i>
+        <h3>Code block</h3>
+        <Code highlightedLines={highlightedLines}>{codeBlockContents}</Code>
+      </Layout>
+    </div>
+  )
+}
