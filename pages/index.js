6b7f0c96b09a2735637bf1d40e7571160443b1f3
rhyolight <matt@numenta.org>
2019-04-23 14:48:36 -0700

    Added some info on landing page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,15 @@ const Index = () => (
   <div>
     <Layout>
       <h1>Building HTM Systems</h1>
+      <p>
+        This website is under active development on <a href="https://www.twitch.tv/rhyolight_">Twitch</a>.
+      </p>
+      <ul>
+        <li><a href="https://www.twitch.tv/rhyolight_/events">livestream schedule</a></li>
+        <li><a href="https://github.com/htm-community/building-htm-systems">github</a></li>
+        <li><a href="https://github.com/htm-community/simplehtm">simplehtm</a></li>
+        <li><a href="https://trello.com/b/iOG0iEnT/building-htm-systems ">trello</a></li>
+      </ul>
     </Layout>
   </div>
 )
