0c1dd4546230940864bf23b2a2dd2e07f66b9b2c
rhyolight <matt@numenta.org>
2019-05-02 10:44:41 -0700

    Added encoding numbers content

--- pages/encoding-numbers.js
+++ pages/encoding-numbers.js
@@ -85,6 +85,30 @@ const encodingOne = `[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
   }
   return out
 }`
+const codeExampleThree = `let encoder = new BoundedScalarEncoder({
+  min: 0, max: 50,
+  w: 10, n: 100
+})
+function onNewValue(value) {
+  let encoding = encoder.encode(value)
+}`
+const codeExampleFour = `applyBitmaskAtIndex(encoding, index) {
+  for (let i = 1; i < w; i++) {
+      let bitIndex = index + i
+      // Adjust for out of range, by cycling around
+      if (bitIndex >= n) bitIndex = bitIndex - n
+      encoding[bitIndex] = 1
+  }
+  return encoding
+}`
+const codeExampleFive = `let values = [0,1,2,3,4]
+let w = 5
+let encoder = new CyclicScalarEncoder({
+    w: w,
+    n: values.length * w,
+    min: 0,
+    max: values.length
+})`
 
 export default function EncodingNumbers() {
   return (
@@ -129,8 +153,9 @@ export default function EncodingNumbers() {
           bits={50}
           width={500}
         />
-
-        <span><a href="#simpleScalarEncoder">¶</a>Figure 1:</span> A value between 0 and 55 is encoded into bits above. Move your mouse over the number line to see the encoding update. Hover over the bits in the encoding to see the value range each bit can represent.
+        <div>
+          <span><a href="#simpleScalarEncoder">¶</a>Figure 1:</span> A value between 0 and 55 is encoded into bits above. Move your mouse over the number line to see the encoding update. Hover over the bits in the encoding to see the value range each bit can represent.
+        </div>
 
         <p>Notice how the range of the on bits in the encoding always encompass the currently selected value. As you move towards the min and max values, you might notice there is a problem with this representation. If you increase the resolution and move the value, you can clearly see the number of bits in the representation decreasing by half as you approach the min and max values (watch the figure above as you <a>click here</a>). Did you notice anything? <a>Click again</a> and pay attention to the size of the encoding. It changes as the value moves toward the edge, and that breaks one of our <a href="/encoders"> established earlier</a>. Principle #4 of encoders states:</p>
         <blockquote>The output should have similar sparsity for all inputs and have enough one-bits to handle noise and subsampling.</blockquote>
@@ -151,7 +176,100 @@ export default function EncodingNumbers() {
           bits={50}
           width={500}
         />
-        <span><a href="#boundedScalarEncoder">¶</a>Figure 2:</span> Unlike the encodings in <em>Figure 1</em>, the size of all output encodings in this example will be the same because we have manually bounded the edges to force the representation to have a constant sparsity.
+        <div>
+          <span><a href="#boundedScalarEncoder">¶</a>Figure 2:</span> Unlike the encodings in <em>Figure 1</em>, the size of all output encodings in this example will be the same because we have manually bounded the edges to force the representation to have a constant sparsity.
+        </div>
+
+        <h3 id="complete-code-reference">Complete Code Reference <a href="#complete-code-reference">¶</a></h3>
+        <p>See the complete <code><a href="https://github.com/htm-community/simplehtm/blob/master/src/encoders/scalar.js" rel="noopener" target="_blank">ScalarEncoder</a></code> and <code><a href="https://github.com/htm-community/simplehtm/blob/master/src/encoders/boundedScalar.js" rel="noopener" target="_blank">BoundedScalarEncoder</a></code> JavaScript classes used in these examples. As an example, the following configuration produces the behavior visualized below.</p>
+
+        <div id="code-example-3">
+          <CodeSyntax>{codeExampleThree}</CodeSyntax>
+        </div>
+        <div>
+          <span><a href="#code-example-3">¶</a>Code Example 3:</span>An example of the creation of an encoder and its usage.
+        </div>
+
+        <DiagramStub 
+          id="exampleBoundedScalarEncoder" 
+        />
+        <span><a href="#exampleBoundedScalarEncoder">¶</a>Figure 3:</span> The behavior of a bounded encoder with a continuous input range of <code>0-50</code> into a bit range of <code>10</code> on bits in a <code>100</code>-bit array.
+
+        <h3 id="output-parameters">Output Parameters<a href="#output-parameters">¶</a></h3>
+        <p>Encoders should give users control over the size and sparsity of encoders they create. Given constant values for the input range of 0-100, change the <code>w</code> and <code>n</code> values in the visualization below and observe how the output encoding changes. </p>
+        <DiagramStub 
+          id="outputRange" 
+        />
+        <span><a href="#outputRange">¶</a>Figure 4:</span> This visual allows you to change the number of bits in the entire encoding (<code>n</code>) and the number of bits on (<code>w</code>).
+
+
+        <h3 id="encoding-by-min-max">Encoding by min / max<a href="#encoding-by-min-max">¶</a></h3>
+        <p>If you know the input domain for an encoder will remain constant, the easiest way to create an encoder is by defining a minimum and maximum input range. Once an encoder is created, these values cannot be changed or else encodings will be inconsistent. To see what an encoder configuration by min/max values might be like, change the <code>min</code> and <code>max</code> values in the panel below.</p>
+        <DiagramStub 
+          id="byMinMaxScalarEncoder" 
+        />
+        <span><a href="#byMinMaxScalarEncoder">¶</a>Figure 5:</span> Define the input range with <code>min</code> and <code>max</code>.
+
+
+        <h3 id="encoding-by-bit-resolution">Encoding by bit resolution<a href="#encoding-by-bit-resolution">¶</a></h3>
+        <p>It might make more sense to create an encoder based upon the range of values each bit in the output array can represent. That is what we mean by <code>resolution</code>, the range of input values one bit represents in the output space.</p>
+        <DiagramStub 
+          id="byResolutionScalarEncoder" 
+        />
+        <span><a href="#byResolutionScalarEncoder">¶</a>Figure 6:</span> The <code>resolution</code> is the range of values that one output bit represents.
+        <p>The higher the <code>resolution</code>, the larger the input range. This makes sense when you think about each bit containing a larger range of values. For this example, I simply hard-coded the <code>min</code> value to be zero and updated the encoder's <code>max</code> based upon the <code>resolution</code> value.</p>
+
+        <h2 id="cyclic-encoding">Cyclic Encoding<a href="#cyclic-encoding">¶</a></h2>
+        <p>Remember above when we had to deal with the special cases of values being encoded at the beginning and end of the value range so all representations were the same size in the output array? Another way to deal with that is by assuming the entire output array is a continuous space -- that it wraps around from the end back to the beginning. We can do this simply by changing how the bitmask around the target index is created:</p>
+
+        <div id="code-example-4">
+          <CodeSyntax>{codeExampleFour}</CodeSyntax>
+        </div>
+        <span><a href="#code-example-4">¶</a>Code Example 4:</span> The only code we need to override is the code that applies the bitmask at the specified index.
+
+        <p>Wow, our <code><a href="https://github.com/htm-community/simplehtm/blob/master/src/encoders/cyclicScalar.js" target="_blank">CyclicScalarEncoder</a></code> is the simplest one so far! As a value starts to approach the end of the encoding space, bits in the beginning of the array will activate and the value will <a>loop through the array as a value changes</a>. In the figure below, mouse over the line towards the max value and watch as the bits wrap to the beginning of the array.</p>
+
+        <DiagramStub 
+          id="cyclicEncoder" 
+        />
+        <span><a href="#cyclicEncoder">¶</a>Figure 7:</span> Because the block of on bits wraps as you approach the end of this array, it is natural to view this as a circle by choosing the `circle` display option above.
+
+        <p>Change the display option in the visualization above to <code>circle</code>. When viewed in this way, the wrapping of the output bit makes more sense. Change the value being encoded by mousing over the value line above and observe the encoding. </p>
+
+        <p>We'll be taking strong advantage of the simplicity of cyclic encoding when we talk about <a href="/encoding-time">encoding periods of time</a>, as well as in the next section, when we talk about <a href="/encoding-categories">category encoding</a>.</p>
+
+        <h2 id="discrete-vs-continuous">Discrete Vs Continuous<a href="#discrete-vs-continuous">¶</a></h2>
+
+        <p>All the examples shown so far have been of continuous encodings, because all our input ranges have been a continuous scale of numeric values. This also means that numbers near one another on the number line have been represented similarly. For example, in <strong>Figure 8</strong> below you can see encodings for two numbers: <code>4</code> and <code>5</code>. Given the encoding parameters defined below, you can see the overlapping bits in green.</p>
+
+        <DiagramStub 
+          id="continuousOverlap" 
+        />
+        <span><a href="#continuousOverlap">¶</a>Figure 8:</span> The encoding of <code>4</code> is in blue and <code>5</code> is in yellow. The overlapping bits are green.
+
+
+        <p>This overlap means that these to values are semantically similar. They are represented similarly because their values are close on the number line. Compare the overlap of <a>two close values</a> vs two values farther from each other. <a>These two values</a> are far enough away from each other on the number line that they have no semantic similarity. Try changing the values of <code>w</code> and <code>n</code> while noticing how it affects the overlap between <a>close</a> and <a>far</a> values.</p>
+
+        <p>Continuous encoding is great for ranges of input values, but sometimes we don't want encodings to overlap. You might want to separate an encoding space into equal sections that encode different categories of data, like this:</p>
+
+        <DiagramStub 
+          id="discreteEncoding" 
+        />
+        <span><a href="#discreteEncoding">¶</a>Figure 9:</span> By limiting the input to discrete values and making <code>n</code> an even multiple of <code>w</code>, it is easy to encode discrete scalar values with a <code>CyclicScalarEncoder</code>.
+
+
+
+        <p>Accomplishing this is really quite simple if we use logic we've already defined for the <code><a href="https://github.com/htm-community/simplehtm/blob/master/src/encoders/cyclicScalar.js" target="_blank">CyclicScalarEncoder</a></code>. If we know how many different discrete values we need to encode and the number of bits to use for each, we can do this:</p>
+
+        <div id="code-example-5">
+          <CodeSyntax>{codeExampleFive}</CodeSyntax>
+        </div>
+        <span><a href="#code-example-5">¶</a>Code Example 5:</span>Use the <code>CyclicScalarEncoder</code>.
+
+        <p>This <code><a href="https://github.com/htm-community/simplehtm/blob/master/src/encoders/cyclicScalar.js" target="_blank">CyclicScalarEncoder</a></code> is now configured to return discrete encodings for the discrete input values <code>[0,1,2,3,4]</code>, but remember we have to send it integers, not decimal values in order to get back non-continuous encodings. Watch us turn this little code snippet into a <code>CategoryEncoder</code> in the <a href="/encoding-categories">next section</a>.</p>  
+
+        <hr />
+        <strong>Next: <a href="/encoding-categories">Encoding Categories</a></strong>
 
       </Layout>
     </div>
