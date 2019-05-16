7645ee3ae131d7190ca465ddebfede17bbbf8b18
rhyolight <matt@numenta.org>
2019-05-16 14:27:21 -0700

    Spaces to tabs

--- pages/encoding-time.jsx
+++ pages/encoding-time.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 export default function EncodingTime() {
-  return (
-    <div>
-      <Layout>
-        <h2>Encoding Time</h2>
-      </Layout>
-    </div>
-  )
+	return (
+		<div>
+			<Layout>
+				<h2>Encoding Time</h2>
+			</Layout>
+		</div>
+	)
 }
